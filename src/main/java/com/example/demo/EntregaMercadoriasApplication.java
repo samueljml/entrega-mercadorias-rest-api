423eaf978c05b8320813ebd7130e7b553858f024
Samuel Andrade <samuelfigueiredodeandrade@gmail.com>
2021-05-19 14:03:08 -0300

    Criando projeto

---
+++ src/main/java/com/example/demo/EntregaMercadoriasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaMercadoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaMercadoriasApplication.class, args);
+	}
+
+}
